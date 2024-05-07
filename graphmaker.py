657ecd9599a3c50b10b331db4f71c488d793aecd
lucasviner <101245656+lucasviner@users.noreply.github.com>
2024-05-07 12:19:16 +0200

    nou graphmaker

--- graphmaker.py
+++ graphmaker.py
@@ -1,7 +1,29 @@
 import networkx as nx
+from sklearn.cluster import KMeans
+
 
 def make_graph(segments: Segments, clusters: int) -> nx.Graph:
     """Make a graph from the segments."""
+    vertices = vertexs()
+    arestes = 
 
+    
 def simplify_graph(graph: nx.Graph, epsilon: float) -> nx.Graph:
     """Simplify the graph."""
+  
+def vertexs(dades: list[tuple(int, int)]) -> list[list[int, int]]:
+    # Definimos el número de clusters que queremos
+    n_clusters = 2 #Donat per l'usuari
+
+    # Inicializamos el modelo de KMeans
+    kmeans = KMeans(n_clusters = n_clusters)
+
+    # Entrenamos el modelo con nuestros datos
+    kmeans.fit(dades)
+
+    # Obtenemos los centroides
+    centroides: list[list[int, int]] = kmeans.cluster_centers_
+
+    # Obtenemos las etiquetas de cluster para cada punto
+    etiquetas = kmeans.labels_
+    return centroides
