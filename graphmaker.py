10d6ac63e21f3d1f3f74065def256c3cf6f7c7e7
oriolparent <167958211+oriolparent@users.noreply.github.com>
2024-05-13 19:33:25 +0200

    Add files via upload

--- graphmaker.py
+++ graphmaker.py
@@ -2,18 +2,19 @@ import networkx as nx
 from sklearn.cluster import KMeans
 from segments import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def make_graph(segments: list[Segment], clusters: int) -> nx.Graph:
     """Make a graph from the segments."""
-    punts = segments_a_punts(segments)
+    punts = segments_a_numpy(segments)
     kmeans = KMeans(n_clusters = clusters)
-    kmeans.fit(np.array(punts))
+    kmeans.fit(punts)
     centroides = kmeans.cluster_centers_ # retorna una tripleta amb el numero de centroid i la tupla altitud latitud
     etiquetas = kmeans.labels_ # llista de enters que diu a quin centroid pertany cada punt
     graf = nx.Graph()
-    for i, centroid in list(enumerate(centroides)):
-        graf.add_node(i,pos = (centroid[0],centroid[1]))
+    for i, centroid in enumerate(centroides):
+        graf.add_node(i,pos = centroid)
     aristas = arestes(etiquetas)
     graf.add_edges_from(aristas)
 
@@ -22,14 +23,20 @@ def make_graph(segments: list[Segment], clusters: int) -> nx.Graph:
 '''def simplify_graph(graph: nx.Graph, epsilon: float) -> nx.Graph:
     """Simplify the graph."""'''
     
-def segments_a_punts(segments: list[Segment]) -> np.array:
-    return np.array([[segment.start, segment.end] for segment in segments])
+def segments_a_numpy(segments: list[Segment]) -> np.array:
+    points = []
+    for segment in segments:
+        points.append(segment.start)
+        points.append(segment.end)
+    points_array = np.array([[point.lat, point.lon] for point in points])
+    return points_array
 
 def arestes(etiquetas:list[int]) -> list[tuple[int,int]]: # llista de cluster de sortida i d'eentrada
     arestes_Centroides: list[tuple[int,int]] = []
     for i in range(0,len(etiquetas),2):
         if etiquetas[i] != etiquetas[i+1]:
-            arestes_Centroides.append((etiquetas[i],etiquetas[i+1]))
+            arestes_Centroides.append([etiquetas[i],etiquetas[i+1]])
     return arestes_Centroides
         
-print(make_graph(load_segments("filename.txt"), 10))
+nx.draw(make_graph(), with_labels=True)
+plt.show()
