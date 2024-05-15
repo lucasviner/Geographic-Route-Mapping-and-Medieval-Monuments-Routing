bddab1c2949fdf0fcf2b43a063a012e69627480e
lucasviner <101245656+lucasviner@users.noreply.github.com>
2024-05-15 18:04:57 +0200

    Add files via upload

--- monuments.py
+++ monuments.py
@@ -28,38 +28,56 @@ def download_monuments(filename: str) -> Monuments:
         monument_url = link.get("href")
         monument_response = requests.get(monument_url)
         monument_soup = BeautifulSoup(monument_response.content, "html.parser")
-        location_element = monument_soup.find(
-            lambda tag: tag.name == "p" and "Localització:" in tag.text
-        )
+        location_element = monument_soup.find(lambda tag: tag.name == "p" and "Localització:" in tag.text)
         if location_element:
             lat, lon = extreure_coordenades(location_element.text.strip())
-            if lat != "-1" and lon != "-1":
+            if lat != -1 and lon != -1:
                 f.write(f"{monument_name},{float(lat)},{float(lon)}\n")
                 monuments.append(Monument(monument_name, Point(float(lat), float(lon))))
     f.close()
     return monuments
 
 
-def extreure_coordenades(dades: str) -> tuple[str, str]:
+def extreure_coordenades(dades: str) -> tuple[float, float]:
     """
     Retorna una tupla de dos strings que conté la longitud i la latitud d'un seguit de dades. Nomes la tindrem en compte si segueix el
     format general de la majoria de dades --> "Longitud: N <lat> <min> <seg> E <lon> <min> <seg>."
     En qualsevol cas alternatiu no es tindrà en compte el monument i la tupla serà de -1's.
     """
-    lat, lon = "-1", "-1"
+    lat, lon = -1.0, -1.0
     parts = dades.split()
     try:
-        if (
-            len(parts) <= 13
-            and parts[0] == "Localització:"
-            and parts[1] == "N"
-            and parts[5] == "E"
-        ):
-            lat = parts[2]
-            lon = parts[6]
+        if (len(parts) <= 13 and parts[0] == "Localització:" and parts[1] == "N" and parts[5] == "E"):
+            lat, lon = parse_coordinates(dades)
+        return lat, lon
     except:
         return lat, lon
+
+
+def parse_coordinates(coords_str: str) -> tuple[float, float]:
+    """
+    Parsea una cadena de coordenadas en formato DMS (grados, minutos, segundos) y devuelve un punto con coordenadas en grados decimales.
+    """
+    parts = coords_str.split()
+    lat_deg = float(parts[2])
+    lat_min = float(parts[3])
+    lat_sec = float(parts[4])
+    lon_deg = float(parts[6])
+    lon_min = float(parts[7])
+    lon_sec = float(parts[8])
+    lat = dms_to_decimal(lat_deg, lat_min, lat_sec, parts[1])
+    lon = dms_to_decimal(lon_deg, lon_min, lon_sec, parts[5])
     return lat, lon
+
+
+def dms_to_decimal(degrees: float, minutes: float, seconds: float, direction: str) -> float:
+    """
+    Convierte coordenadas en grados, minutos y segundos a grados decimales.
+    """
+    decimal_degrees = degrees + minutes / 60 + seconds / 3600
+    if direction in ['S', 'W']:
+        decimal_degrees *= -1
+    return decimal_degrees
 
 
 def load_monuments(filename: str) -> Monuments:
@@ -99,4 +117,4 @@ def save_monuments(monuments: Monuments, filename: str) -> None:
 
 # COMPROVACIÓ
 
-print(get_monuments("filenamee.txt"))
+print(download_monuments("filenamee.txt"))
--- routes.py
+++ routes.py
@@ -1,12 +1,58 @@
+from typing import TypeAlias
+from dataclasses import dataclass
+import networkx as nx
+from segments import Point, load_segments
+from monuments import Monuments, load_monuments
+from fastkml import *
+from shapely.geometry import Point
+from staticmap import *
+from graphmaker import make_graph
+
+
+@dataclass
 class Routes:
+    path: list[Point]
+
+paths: TypeAlias = list[list[Point]]
+
 
 def find_routes(graph: nx.Graph, start: Point, endpoints: Monuments) -> Routes:
-    """Find the shortest route between the starting point and all the endpoints."""
-
+    """ Find the shortest route between the starting point and all the endpoints. """
+    routes = []
+    for endpoint in endpoints:
+        try:
+            route = nx.shortest_path(graph, source=start, target=endpoint.location)
+            routes.append(route)
+        except nx.NetworkXNoPath:
+            print(f"No path found from {start} to {endpoint.location}.")
+    return Routes(path = routes)
 
 def export_PNG(routes: Routes, filename: str) -> None:
     """Export the graph to a PNG file using staticmaps."""
+    static_map = StaticMap(800, 600)
 
+    for path in routes.path:
+        for i in range(len(path) - 1):
+            start_point = path[i]
+            end_point = path[i + 1]
+            line = Line(((start_point.lon, start_point.lat), (end_point.lon, end_point.lat)), 'blue', 2)
+            static_map.add_line(line)
+
+    image = static_map.render()
+    image.save(filename)
 
 def export_KML(groutes: Routes, filename: str) -> None:
     """Export the graph to a KML file."""
+    kml = KML()
+    
+    for path in routes.path:
+        line = kml.newlinestring(name="Route", description="Shortest route", coords=[(point.lon, point.lat) for point in path])
+        line.style.linestyle.width = 4
+        line.style.linestyle.color = Color.blue
+    
+    kml.save(filename)
+
+segments = load_segments('filename.txt')
+G = make_graph(segments, 90)
+routes = find_routes(G, Point(41.7215,1.8296944444444445), load_monuments('filenamee.txt'))
+export_PNG(routes, 'graf.png')
