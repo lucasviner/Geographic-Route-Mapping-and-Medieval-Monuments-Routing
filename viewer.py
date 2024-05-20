42f762ac798522f171cec65ccf30fa8f4c0eab5b
oriolparent <167958211+oriolparent@users.noreply.github.com>
2024-05-20 11:03:10 +0200

    Add files via upload

--- viewer.py
+++ viewer.py
@@ -9,6 +9,13 @@ def export_PNG(graph: nx.Graph, filename: str) -> None:
     """Export the graph to a PNG file using staticmap."""
     static_map = StaticMap(800, 600)
     #AIXO HAURIA DE ANAR EN UNA FUNCIÓ
+    arestes_PNG(graph, static_map)
+    nodes_PNG(graph, static_map)
+    imatge = static_map.render()
+    imatge.save(filename)
+
+def arestes_PNG(graph: nx.Graph, static_map: StaticMap)-> None:
+    """Adds the edges of a graph to a StaticMap as Lines"""
     for edge in graph.edges():
         start_lat = graph.nodes[edge[0]]["pos"][0]
         start_lon = graph.nodes[edge[0]]["pos"][1]
@@ -16,14 +23,13 @@ def export_PNG(graph: nx.Graph, filename: str) -> None:
         end_lon = graph.nodes[edge[1]]["pos"][1]
         line = Line([(start_lat, start_lon), (end_lat,end_lon)], "blue", 2)
         static_map.add_line(line)
-    #AIXO EN UN ALTRE
+
+def nodes_PNG(graph: nx.Graph, static_map: StaticMap)->None:
+    """Adds the nodes of a graph to a StaticMap as CircleMarkers"""
     for node in graph.nodes():
         pos = graph.nodes[node]["pos"]
         vertex = CircleMarker(pos, "red", 6)
         static_map.add_marker(vertex)
-
-    imatge = static_map.render()
-    imatge.save(filename)
 
 def export_KML(graph: nx.Graph, filename: str) -> None:
     """Export the graph to a KML file."""
@@ -31,15 +37,22 @@ def export_KML(graph: nx.Graph, filename: str) -> None:
     namespace = '{http://www.opengis.net/kml/2.2}'
     document = Document(namespace, 'docid', 'Graph', 'Graph KML')
     kml.append(document)
-    #AIXO HAURIA DE ANAR EN UNA FUNCIÓ
+    nodes_KML(graph, document, namespace)
+    arestes_KML(graph, document, namespace)
+    obrir_fitxer(filename, kml)
+
+def nodes_KML(graph: nx.Graph, document: Document, namespace: str)->None:
+    """Add the nodes of a graph to a KML as Placemarks"""
     for node in graph.nodes():
-        lat = graph.nodes[node]['pos'][0]
-        lon = graph.nodes[node]['pos'][1]
-        point = Point(float(lat),float(lon)) 
-        placemark = Placemark(namespace, str(node), str(node))
-        placemark.geometry = point
-        document.append(placemark)
-    #AIXO EN UN ALTRE
+            lat = graph.nodes[node]['pos'][0]
+            lon = graph.nodes[node]['pos'][1]
+            point = Point(float(lat),float(lon)) 
+            placemark = Placemark(namespace, str(node), str(node))
+            placemark.geometry = point
+            document.append(placemark)
+
+def arestes_KML(graph: nx.Graph, document: Document, namespace: str)->None:
+    """Adds the edges of a graph to a KML as Placemarks"""
     for aresta in graph.edges():
         start_pos = graph.nodes[aresta[0]]['pos']
         end_pos = graph.nodes[aresta[1]]['pos']
@@ -47,10 +60,13 @@ def export_KML(graph: nx.Graph, filename: str) -> None:
         placemark = Placemark(namespace, f'edge_{aresta[0]}_{aresta[1]}', f'edge from {aresta[0]} to {aresta[1]}')
         placemark.geometry = line
         document.append(placemark)
-    #AIXO EN UN ALTRE
-        with open(filename, 'w') as fitxer:
+
+def obrir_fitxer(filename: str, kml: KML)->None:
+    """Saves the content of a KML object in a file"""
+    with open(filename, 'w') as fitxer:
             fitxer.write(kml.to_string())
 
+# COMPROVACIÓ
 segments = load_segments('filename.txt')
 graph = make_graph(segments, 100)
 export_PNG(graph, 'graph.png')
