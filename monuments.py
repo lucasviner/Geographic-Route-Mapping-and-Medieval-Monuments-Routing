04a1c06a9535cdf0e29e1ac437df9167e3e60fec
lucasviner <101245656+lucasviner@users.noreply.github.com>
2024-05-24 11:28:14 +0200

    Add files via upload

--- monuments.py
+++ monuments.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import TypeAlias
 from bs4 import BeautifulSoup
 import requests, os
-from segments import Point
+from segments import Point, Box
 from re import findall
 
 
@@ -19,8 +19,7 @@ def download_monuments(filename: str) -> None:
     """ Download monuments from Catalunya Medieval and it saves them to a file. """
     url = "https://www.catalunyamedieval.es/comarques/"
     content = fetch_page_content(url)
-    script_content = extract_script_tag(content, "var aCasaForta")
-    
+    script_content = extract_script_tag(content, "var aCasaForta")    
     if script_content:
         save_monuments_to_file(parse_monument_data(script_content), filename)
 
@@ -63,36 +62,48 @@ def save_monuments_to_file(monuments: list[Monument], filename: str) -> None:
     """Save a list of monuments to a file."""
     with open(filename, 'w') as f:
         for monument in monuments:
-            f.write(f"{monument.name},{monument.location.lat},{monument.location.lon}\n")
+            f.write(f"{monument.name} - {monument.location.lat},{monument.location.lon}\n")
 
 
-def load_monuments(filename: str) -> Monuments:
+def load_monuments(box: Box, filename: str) -> Monuments:
     """Load monuments from a file."""
     monuments: Monuments = []
     with open(filename, "r") as file:    
         for line in file:
             try:
-                monument_name, lat, lon = line.strip().split(",")
-                monuments.append(Monument(monument_name.strip(), Point(float(lat), float(lon))))
-            except ValueError as e:
+                monument_name, lat, lon = get_data_from_file(line)                
+                if monument_in_box(box, lat, lon): 
+                    monuments.append(Monument(monument_name, Point(lat, lon)))
+            except Exception as e:
                 print(f"Error processing line: {line.strip()} - {e}. This line will be ignored.")
-                continue
     return monuments
 
 
-def get_monuments(filename: str) -> Monuments:
+def get_data_from_file(line: str) -> tuple[str, float, float]:
+    """ Gets the monument name and the coordenates from the file """
+    name_and_coords = line.strip().split(" - ")
+    monument_name = name_and_coords[0]
+    lat, lon = name_and_coords[1].split(",")
+    return monument_name, float(lat), float(lon)
+                 
+
+def monument_in_box(box: Box, lat: float, lon: float) -> bool:
+    """ Returns if the monument is inside the interested box. """
+    return box.bottom_left.lat <= lat <= box.top_right.lat and box.bottom_left.lon <= lon <= box.top_right.lon
+
+def get_monuments(box: Box, filename: str) -> Monuments:
     """
     Get all monuments in the box.
     If filename exists, load monuments from the file.
     Otherwise, download monuments and save them to the file.
     """
-    if os.path.exists(filename):
-        return load_monuments(filename)
-    download_monuments(filename)
-    return load_monuments(filename)
+    if not os.path.exists(filename):
+        download_monuments(filename)
+    return load_monuments(box,filename)
+    
 
 
 # COMPROVACIÓ
-#if __name__ == "__main__":
-    #download_monuments("filenamee.txt")
-    #load_monuments("filenamee.txt")
+
+#download_monuments("monuments.dat")
+#print(get_monuments(Box(Point(40.5363713, 0.5739316671), Point(40.79886535, 0.9021482)), "monuments.dat"))
