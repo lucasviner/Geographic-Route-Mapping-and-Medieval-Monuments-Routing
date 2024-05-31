5684c950a2a265005d82d923a7a1c9131d695d56
oriolparent <167958211+oriolparent@users.noreply.github.com>
2024-05-31 21:27:54 +0200

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,10 +1,11 @@
-from segments import Point
-from graphmaker import Graph
-from monuments import Monuments
+import networkx as nx
+from segments import Point, get_segments, Box
+from graphmaker import Graph, make_graph
+from monuments import Monuments, get_monuments
 from typing import TypeAlias, Optional
 from staticmap import StaticMap, CircleMarker, Line
-import networkx as nx
-import simplekml, haversine
+from math import *
+import simplekml
 
 
 Routes: TypeAlias = dict[str, list[int]]
@@ -25,66 +26,80 @@ def find_routes(graph: Graph, start_point: Point, monuments: Monuments, filename
         print('No monuments found in the selected box.')
         return -1
 
+
 def find_closest_node(graph: Graph, point: Point) -> int:
     """Find the closest node in the graph to a given point."""
     closest_node = None
     min_distance = float('inf')
     for node, data in graph.nodes(data=True):
-        lat, lon = data['pos']
-        distance = haversine.haversine((point.lat, point.lon), (lat, lon))
+        lat, lon = data['pos'][1], data['pos'][0]
+        distance = haversine_distance(point, Point(lat, lon))
         if distance < min_distance:
             min_distance = distance
             closest_node = node
-    
-    if closest_node is None:
-        raise ValueError("No nodes found in the graph")
-    
     return closest_node
+
+
+def haversine_distance(point1: Point, point2: Point) -> float:
+    """Calculate the Haversine distance between two points."""
+    R = 6371.0  # Radius of the Earth in kilometers
+    lat1, lon1 = convert_to_radians(point1.lat, point1.lon)
+    lat2, lon2 = convert_to_radians(point2.lat, point2.lon)
+    dlat, dlon = lat2 - lat1, lon2 - lon1
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = R * c # Distance in kilometers
+    
+    return distance
+
+
+def convert_to_radians(lat: float, lon: float) -> tuple[float, float]:
+    """Convert latitude and longitude from degrees to radians."""
+    return radians(lat), radians(lon)
 
 
 def get_monuments_nodes(G: Graph, monuments: Monuments) -> set[int]:
     """Get the set of nodes corresponding to the monuments."""
     return {find_closest_node(G, monument.location) for monument in monuments}
+
+
+def map_monuments_to_nodes(graph: Graph, monuments: Monuments) -> dict[str, int]:
+    """Map each monument to the closest node in the graph."""
+    return {monument.name: find_closest_node(graph, monument.location) for monument in monuments}
       
 
 def contains_node(monument_nodes: set, shortest_paths) -> bool:
-    """Check if any monument node is present in the shortest paths."""
+    """Returns if it contains nodes"""
     return any(monument_node in path for path in shortest_paths[1].values() for monument_node in monument_nodes)
-
-
-def build_route_graph(G: Graph, monument_nodes: set, shortest_paths) -> Graph:
-    """Build a graph containing only the nodes and edges of the shortest paths to monuments."""
-    route_graph = nx.Graph()
-    for monument_node in list(monument_nodes):
-        for path in list(shortest_paths[1].values()):
-            if monument_node in path:
-                path = list(shortest_paths[1][monument_node])
-                route_graph = add_nodes_and_edges(G, path)
-
-    return route_graph
-
-
-def add_nodes_and_edges(G: Graph, path) -> Graph:
-    """Add nodes and edges to the route graph."""
-    route_graph = nx.Graph()
-    for i in range(len(path) - 1):
-        u, v = path[i], path[i + 1]
-        point1 = get_node_position(G, u)
-        point2 = get_node_position(G, v)
-        if point1 and point2:
-            lat1, lon1 = point1
-            lat2, lon2 = point2
-            weight = haversine.haversine((lat1, lon1), (lat2, lon2))
-            route_graph.add_edge(u, v, weight=weight)
-            route_graph.add_node(u, pos=(lat1,lon1))
-            route_graph.add_node(v, pos=(lat2,lon2))
-    return route_graph
 
 
 def get_node_position(G: Graph, node: int) -> Optional[tuple[float, float]]:
     """Get the position of a node in the graph."""
     return G.nodes[node]['pos'] if node in G.nodes else None
 
+
+def build_route_graph(G: Graph, monument_nodes: set, shortest_paths) -> Graph:
+    """Build a graph containing only the nodes and edges of the shortest paths to monuments."""
+    route_graph = nx.Graph()
+    for monument_node in list(monument_nodes):
+            for path in list(shortest_paths[1].values()):
+                if monument_node in path:
+                    path = list(shortest_paths[1][monument_node])
+                    add_nodes_and_edges(G, route_graph, path)
+    return route_graph
+
+
+def add_nodes_and_edges(G: Graph, route_graph: Graph, path)-> None:
+    for i in range(len(path) - 1):
+        u, v = path[i], path[i + 1]
+        lat1, lon1 = get_node_position(G,u)[0], get_node_position(G,u)[1]
+        lat2, lon2 = get_node_position(G,v)[0], get_node_position(G,v)[1]
+        weight = haversine_distance(Point(lat1, lon1), Point(lat2, lon2))
+        route_graph.add_edge(u, v, weight=weight)
+        route_graph.add_node(u, pos=(lat1,lon1))
+        route_graph.add_node(v, pos=(lat2,lon2))
+        
+    return route_graph
 
 def save_static_map(G: Graph, start_node: int, monument_nodes: set[int], filename: str) -> None:
     """Generate and save a static map image."""
@@ -137,3 +152,10 @@ def add_edges_to_kml(graph: Graph, kml: simplekml.Kml) -> None:
         line = kml.newlinestring(name=f"{u}-{v}", coords=[(pos_u[0], pos_u[1]), (pos_v[0], pos_v[1])])
         line.style.linestyle.color = 'ff000000'  # Black
         line.style.linestyle.width = 2
+
+monuments = get_monuments(Box(Point(40.5363713, 0.5739316671), Point(40.79886535, 0.9021482)),"monuments.dat")
+segments = get_segments(Box(Point(40.5363713, 0.5739316671), Point(40.79886535, 0.9021482)), "segments.dat")
+G = make_graph(segments, 100)
+start_point = Point(lat=40.55, lon=0.6739316671)
+
+find_routes(G, start_point, monuments, 'routes')
